422b508d0771123d6c61868cc27792a1da6af49f
carol-wambui <carolwambuidaystar@gmail.com>
2021-01-27 14:00:25 +0300

    add user

---
+++ user/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from .models import Profile
+from django.contrib.auth.models import User
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='Mabele')
+        # self.user.save()
+        self.profile_test = Profile(user=self.user, image='default.jpg' )
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, Profile))
